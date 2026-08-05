18692faa0ef8ee8587ab7575c3aebc3ac3118f0b
Molly Lindquist <mollymlindquist@gmail.com>
2026-08-05 08:12:33 -0700

    Fix root outputs: ACR moved to shared config

--- outputs.tf
+++ outputs.tf
@@ -3,12 +3,3 @@ output "app_url" {
   value       = "https://${azurerm_container_app.this.ingress[0].fqdn}"
 }
 
-output "acr_login_server" {
-  description = "Registry address for docker tag/push (e.g. acrcontainerdemomolly.azurecr.io)."
-  value       = azurerm_container_registry.this.login_server
-}
-
-output "acr_name" {
-  description = "Registry name for az acr login."
-  value       = azurerm_container_registry.this.name
-}
